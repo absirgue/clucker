b91aee18058a5ac6103d92001f7dcdb6d5fd49e2
absirgue <anton.sirgue@kcl.ac.uk>
2022-10-04 14:16:39 +0100

    Create User model

--- clucker/settings.py
+++ clucker/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#this is to make our own user model oveeride the pre-implemented one and be used for authentication
+AUTH_USER_MODEL = 'microblogs.User'
--- microblogs/models.py
+++ microblogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+# We import an abstract user class that we will be able to override to have our own model (see Django doc)
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField()
+     

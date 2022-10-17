0a12ee9cf66e2a2a30892fc2de075900573f0367
absirgue <anton.sirgue@kcl.ac.uk>
2022-10-17 22:46:55 +0100

    Test that valid user is active

--- microblogs/tests/test_log_in_view.py
+++ microblogs/tests/test_log_in_view.py
@@ -10,7 +10,7 @@ class LogInViewTestCase(TestCase,LogInTester):
 
     def setUp(self):
         self.url = reverse('log_in')
-        User.objects.create_user('@johndoe', first_name="John", last_name= "Doe", email="jdoe@example.com", bio="Hi I'm John!", password='Password123')
+        self.user = User.objects.create_user('@johndoe', first_name="John", last_name= "Doe", email="jdoe@example.com", bio="Hi I'm John!", password='Password123', is_active=True)
         self.form_input = {
         "username" : "@janedoe",
         "password" : "Password123",
@@ -51,3 +51,20 @@ class LogInViewTestCase(TestCase,LogInTester):
         response_url = reverse('feed')
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response,'feed.html')
+
+    def test_valid_log_in_inactive_user(self):
+        self.user.is_active = False
+        self.user.save()
+        form_input = {
+        "username" : "@johndoe",
+        "password" : "Password123",
+        }
+        response = self.client.post(self.url, form_input, follow=True)
+        response_url = reverse('feed')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        # In an unsuccessful log in screen, we want the form to not be bound so the user has to enter from scratch again
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
